1fa98c66163dd63bbfd11918629b90ae304aa6f1
Simon Gustavsson <simgu324@student.liu.se>
2019-02-25 11:42:41 +0100

    added FileBrowser

---
+++ stablab/Assets/Scripts/FileBrowserExample.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+using Crosstales.FB;
+
+public class FileBrowserExample : MonoBehaviour
+{
+   
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            string path = FileBrowser.SaveFile("MyFile", "txt");
+            Debug.Log("Save file: " + path);
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            string path = GetFolderPath();
+            Debug.Log("Selected file: " + path);
+        }
+    }
+
+    string GetFilePath() 
+    { 
+        return FileBrowser.OpenSingleFile("Open single file", "txt", "jpg", "pdf");
+    }
+
+    string GetFolderPath()
+    {
+        return FileBrowser.OpenSingleFolder();
+    }
+
+}
