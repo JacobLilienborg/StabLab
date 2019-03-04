10e30efca808fe84d24a6dd363f63d9bf1cd3a25
Simon Gustavsson <simgu324@student.liu.se>
2019-03-04 17:57:25 +0100

    Create and load projects

--- stablab/Assets/Scripts/FileManager.cs
+++ stablab/Assets/Scripts/FileManager.cs
@@ -6,7 +6,8 @@ using System.Collections.Generic;
 
 public class FileManager : MonoBehaviour
 {
-    private string dataFolderPath;
+    private static string dataFolderPath;
+    private const string DefaultExtension = "data";
 
     void Start()
     {
@@ -15,53 +16,100 @@ public class FileManager : MonoBehaviour
         {
             Directory.CreateDirectory(dataFolderPath);
         }
+
+        try
+        {
+            SettingsData settings = LoadAppData<SettingsData>("Settings");
+            if (settings.workingDirectoryPath == SettingsData.defaultFolderName 
+                || !Directory.Exists(settings.workingDirectoryPath))
+            {
+                SetWorkingDirectory();
+            }
+        }
+        catch (FileNotFoundException)
+        {
+            SettingsData settings = new SettingsData();
+            settings.recentProjects = new List<string>();
+            SaveAppData("Settings", settings);
+            SetWorkingDirectory();
+        }
     }
 
-    public void SaveCamera()
+    public static void SetWorkingDirectory()
     {
-        CameraData cam = new CameraData(Camera.main);
-        SaveData<CameraData>("CameraData", cam);
+        SettingsData settings = LoadAppData<SettingsData>("Settings");
+        string path = FileBrowser.OpenSingleFolder("Select location for working directory");
+        string projPath = Path.Combine(path, "StabLab");
+
+        Directory.CreateDirectory(projPath);
+
+        settings.workingDirectoryPath = projPath;
+        SaveAppData("Settings", settings);
     }
 
-    public void LoadCamera()
+    public static string GetWorkingDirectory()
     {
-        CameraData cam = LoadData<CameraData>("CameraData");
-        Camera.main.transform.position = cam.GetPosition();
-        Camera.main.transform.rotation = cam.GetRotation();
-        Camera.main.fieldOfView = cam.GetFieldOfView();
+        SettingsData settings = LoadAppData<SettingsData>("Settings");
+        return settings.workingDirectoryPath;
     }
 
-    // Saves an object of class type T in a binary format in persistentDataPath
-    public void SaveData<T>(string dataName, T data)
+    public static void SaveAppData<T>(string dataName, T data)
+    {
+        SaveData<T>(dataFolderPath, dataName, data);
+    }
+
+    public static T LoadAppData<T>(string dataName)
+    {
+       return LoadData<T>(dataFolderPath, dataName);
+    }
+
+    public static void SaveProjectData<T>(string projName, string dataName, T data)
+    {
+        SaveData<T>(Path.Combine(GetWorkingDirectory(), projName), dataName, data);
+    }
+
+    public static T LoadProjectData<T>(string projName, string dataName)
+    {
+        return LoadData<T>(Path.Combine(GetWorkingDirectory(), projName), dataName);
+    }
+
+    // Saves an object of class type T in a binary format in path
+    private static void SaveData<T>(string path, string dataName, T data)
     {
         BinaryFormatter binaryFormatter = new BinaryFormatter();
-        using (FileStream fileStream = File.Open(Path.Combine(dataFolderPath, dataName), FileMode.OpenOrCreate))
+        using (FileStream fileStream = File.Open(Path.Combine(path, dataName), FileMode.OpenOrCreate))
         {
             binaryFormatter.Serialize(fileStream, data);
         }
     }
 
-    // Returns a loaded object of class type T form a binary format in persistentDataPath
-    public T LoadData<T>(string dataName)
+    // Returns a loaded object of class type T form a binary format in path
+    private static T LoadData<T>(string path, string dataName)
     {
         BinaryFormatter binaryFormatter = new BinaryFormatter();
-        using (FileStream fileStream = File.Open(Path.Combine(dataFolderPath, dataName), FileMode.Open))
+        using (FileStream fileStream = File.Open(Path.Combine(path, dataName), FileMode.Open))
         {
             return (T)binaryFormatter.Deserialize(fileStream);
         }
     }
 
-    // Saves a file
-    public void SaveFile(string fileName)
+    // Saves a file using FileBrowser
+    public static void SaveFileBrowser(string fileName, string extension = DefaultExtension)
     {
-        string path = FileBrowser.SaveFile(fileName, "txt");
+        string path = FileBrowser.SaveFile(fileName, extension);
         Debug.Log("Saved file: " + path);
     }
 
-    public void LoadFile()
+    // Load a file using FileBrowser
+    public static void LoadFileBrowser()
     {
         string path = FileBrowser.OpenSingleFile();
         Debug.Log("Loaded file: " + path);
+    }
+
+    public static string OpenFolderBrowser() 
+    {
+        return FileBrowser.OpenSingleFolder("Select project", GetWorkingDirectory());
     }
 
 
