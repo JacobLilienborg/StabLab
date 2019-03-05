8ddf166acc6f4086b6d19153ad2342897177234a
Martin Jirenius <marji513@student.liu.se>
2019-03-05 10:12:18 +0100

    Working on new project window

---
+++ stablab/Assets/Scripts/Management/InputDrawer.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(List<UserInput>))]
+public class InputDrawer : PropertyDrawer
+{
+    
+}
---
+++ stablab/Assets/Scripts/Management/InputHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class UserInput
+{
+    public delegate bool trigger();
+    private trigger inputFunction;
+    public UnityEvent eventfunctions;
+
+    public UserInput(trigger t)
+    {
+        inputFunction = t;
+    }
+
+    public bool Triggered()
+    {
+        return inputFunction();
+    }
+
+}
+
+public class InputHandler : MonoBehaviour
+{    
+    public List<UserInput> userInputs;
+    
+    void Update()
+    {
+        foreach(UserInput userInput in userInputs)
+        {
+            if (userInput.Triggered()) { userInput.eventfunctions.Invoke(); }
+        }    
+    }
+}
