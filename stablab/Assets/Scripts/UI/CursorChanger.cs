cb6869c3af04015d5dc83789a07026d4f08928d1
Jacob Olausson <jacol637@student.liu.se>
2019-04-01 12:23:20 +0200

    startScreen new look

---
+++ stablab/Assets/Scripts/UI/CursorChanger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorChanger : MonoBehaviour
+{
+    public Texture2D newCursor;
+    public void Start()
+    {
+        // Create a texture. Texture size does not matter, since
+        // LoadImage will replace with with incoming image size.
+    }
+
+    public void OnMouseEnter()
+    {
+        Cursor.SetCursor(newCursor, Vector2.zero, CursorMode.Auto);
+    }
+
+    public void OnMouseExit()
+    {
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+    }
+}
