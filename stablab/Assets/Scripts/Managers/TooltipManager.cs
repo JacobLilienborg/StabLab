cd79be717d323e62a8649c40c2aaa35c1410b609
Viktor Barr <vikba651@student.liu.se>
2019-04-10 16:53:50 +0200

    Måste bara fixa en bug

---
+++ stablab/Assets/Scripts/Managers/TooltipManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TooltipManager : MonoBehaviour
+{
+    static bool show;
+    static string currentInfo;
+    private Text text;
+    private Image image;
+    private bool onePulse = true;
+    void Start()
+    {
+        show = false;
+        text = GetComponentInChildren<Text>();
+        image = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (show)
+        {
+            if (onePulse)
+            {
+                transform.position = Input.mousePosition;
+                onePulse = false;
+            }
+            image.enabled = true;
+            text.enabled = true;
+            text.text = currentInfo;
+        }else
+        {
+            image.enabled = false;
+            text.enabled = false;
+            onePulse = true;
+        }
+    }
+
+    public static void Show(string info)
+    {
+        show = true;
+        currentInfo = info; //Update textContent
+    }
+    public static void Hide()
+    {
+        show = false;
+    }
+}
---
+++ stablab/Assets/Scripts/Misc/TooltipCaller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class TooltipCaller : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField]
+    private string description;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        TooltipManager.Show(description);
+        Debug.Log("SHOW");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        TooltipManager.Hide();
+        Debug.Log("HIDE");
+    }
+}
