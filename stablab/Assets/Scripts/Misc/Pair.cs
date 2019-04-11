c6b621cfb577d599d347bdb7d4ef9ce4ff190210
Jacob Olausson <jacol637@student.liu.se>
2019-04-11 14:28:37 +0200

    gizmos now working, most code from internet source - should be fine, licence MIT -> free to use

---
+++ stablab/Assets/Scripts/Misc/Pair.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pair<T,I> : MonoBehaviour
+{ 
+    public T first { get; set; }
+    public I second { get; set; }
+
+    public Pair(T first, I second)
+    {
+        this.first = first;
+        this.second = second;
+    }
+}
