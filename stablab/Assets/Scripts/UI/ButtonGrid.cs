e0c41daff60c48058d5256c44a4c4d3973d08b46
Jacob Olausson <jacol637@student.liu.se>
2019-05-14 13:06:43 +0200

    ?

---
+++ stablab/Assets/Scripts/UI/ButtonGrid.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonGrid : MonoBehaviour
+{
+    public GameObject area;
+    private int padding;
+    private List<GameObject> gridObjects;
+    private int sizeOfbuttons;
+    private int numCols = 5;
+
+    private void Start()
+    {
+        area.transform
+    }
+
+    public void LoadAllGridObjects() { }
+
+    public void CreateButtonForObject(GameObject o) { }
+
+    public void AddButtonsToGrid() { }
+
+
+
+}
