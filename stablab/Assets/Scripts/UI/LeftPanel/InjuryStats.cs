65559bd64c3ccff0fab9c958760725fc69a72343
Johan Fallström <avgrunden15@gmail.com>
2019-05-16 22:51:53 +0200

    WIP: Städat upp i LeftPanel i PresentationMode

---
+++ stablab/Assets/Scripts/UI/LeftPanel/InjuryStats.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InjuryStats : MonoBehaviour
+{
+
+    public Text index;
+    public Text injuryName;
+
+    private void Update()
+    {
+        if (InjuryManager.activeInjury != null)
+        {
+            index.text      = InjuryManager.injuries.IndexOf(InjuryManager.activeInjury).ToString();
+            if (InjuryManager.activeInjury.Name != null)
+                injuryName.text = InjuryManager.activeInjury.Name;
+            else
+                injuryName.text = "Wound " + index.text;
+        }
+    }
+
+}
