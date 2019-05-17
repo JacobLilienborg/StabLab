fa80caee5659da7a2580182d7981e395b4582af1
Johan Fallström <avgrunden15@gmail.com>
2019-05-17 11:20:55 +0200

    WIP: Fixar kameraknapparna

--- stablab/Assets/Scripts/Controllers/Camera/MovementButtons.cs
+++ stablab/Assets/Scripts/Controllers/Camera/MovementButtons.cs
@@ -12,15 +12,21 @@ enum MOVEMENTTYPE {
     RESET
 };
 
-public class MovementButtons : MonoBehaviour,IPointerDownHandler, IPointerUpHandler
+public class MovementButtons : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
-    private Transform target;
-    public int directionNumber;
-    public int typeNumber;
-    private Vector3 direction;
-    private int speed = 5;
-    private bool move = false;
+    private Transform    target;
+    public  int          directionNumber;
+    public  int          typeNumber;
+    private Vector3      direction;
+    private int          speed    = 5;
+    private bool         move     = false;
     private MOVEMENTTYPE MoveType = MOVEMENTTYPE.PAN;
+
+    private float        yDeg     = 0.0f;
+    private static int   yMin     = -80, 
+                         yMax     = 80;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -28,19 +34,19 @@ public class MovementButtons : MonoBehaviour,IPointerDownHandler, IPointerUpHand
         switch (typeNumber) {
             case 0: //pan
                 MoveType = MOVEMENTTYPE.PAN;
-                speed = 5;
+                speed    = 5;
                 switch (directionNumber) {
                     case 1:
-                        direction = Vector3.left;
-                        break;
-                    case 2:
                         direction = Vector3.right;
                         break;
+                    case 2:
+                        direction = Vector3.left;
+                        break;
                     case 3:
-                        direction = Vector3.down;
+                        direction = Vector3.up;
                         break;
                     case 4:
-                        direction = Vector3.up;
+                        direction = Vector3.down;
                         break;
                     default:
                         direction = Vector3.zero;
@@ -49,7 +55,7 @@ public class MovementButtons : MonoBehaviour,IPointerDownHandler, IPointerUpHand
                 break;
             case 1: //rotation
                 MoveType = MOVEMENTTYPE.ROTATE;
-                speed = 20;
+                speed    = 20;
                 switch (directionNumber)
                 {
                     case 1:
@@ -59,10 +65,10 @@ public class MovementButtons : MonoBehaviour,IPointerDownHandler, IPointerUpHand
                         direction = Vector3.down;
                         break;
                     case 3:
-                        direction = Vector3.left;
+                        direction = Vector3.right;
                         break;
                     case 4:
-                        direction = Vector3.right;
+                        direction = Vector3.left;
                         break;
                     default:
                         direction = Vector3.zero;
@@ -71,7 +77,7 @@ public class MovementButtons : MonoBehaviour,IPointerDownHandler, IPointerUpHand
                 break;
             case 2: // zoom
                 MoveType = MOVEMENTTYPE.ZOOM;
-                speed = 20;
+                speed    = 20;
                 break;
             case 3:
                 MoveType = MOVEMENTTYPE.RESET;
@@ -83,6 +89,10 @@ public class MovementButtons : MonoBehaviour,IPointerDownHandler, IPointerUpHand
     // Update is called once per frame
     void Update()
     {
+        // Without a model, we have nothing to rotate around.
+        if (ModelManager.instance.activeModel == null)
+            return;
+       
         if (move)
         {
             if ((MoveType == MOVEMENTTYPE.PAN))
@@ -91,7 +101,16 @@ public class MovementButtons : MonoBehaviour,IPointerDownHandler, IPointerUpHand
             }
             else if (MoveType == MOVEMENTTYPE.ROTATE)
             {
-                target.RotateAround(Vector3.zero,direction,Time.deltaTime * speed * Camera.main.gameObject.GetComponent<CameraController>().fov / 10);
+                if (direction == Vector3.up || direction == Vector3.down)
+                    target.RotateAround(Vector3.zero, direction, Time.deltaTime * speed * Camera.main.gameObject.GetComponent<CameraController>().fov / 10);
+                else
+                {
+                    if(direction == Vector3.right)
+                        target.RotateAround(Vector3.zero, target.right, Time.deltaTime * speed * Camera.main.gameObject.GetComponent<CameraController>().fov / 10);
+                    else
+                        target.RotateAround(Vector3.zero, target.right*-1, Time.deltaTime * speed * Camera.main.gameObject.GetComponent<CameraController>().fov / 10);
+                    //target.RotateAround(Vector3.zero, direction, Time.deltaTime * speed * Camera.main.gameObject.GetComponent<CameraController>().fov / 10);
+                }
             }
             else if (MoveType == MOVEMENTTYPE.ZOOM)
             {
@@ -99,7 +118,7 @@ public class MovementButtons : MonoBehaviour,IPointerDownHandler, IPointerUpHand
                 fov = Camera.main.fieldOfView;
 
                 fov -= directionNumber * Time.deltaTime * speed;
-                fov = Mathf.Clamp(fov, 0, 100);
+                fov  = Mathf.Clamp(fov, 0, 100);
                 Camera.main.fieldOfView = fov;
             }
         }
@@ -120,4 +139,15 @@ public class MovementButtons : MonoBehaviour,IPointerDownHandler, IPointerUpHand
         target.rotation = Camera.main.gameObject.GetComponent<CameraController>().stdRot;
         Camera.main.fieldOfView = Camera.main.gameObject.GetComponent<CameraController>().stdFov;
     }
+
+    private float LimitAngle(float angle)
+    {
+        if (angle < -360)
+            angle += 360;
+        if (angle > 360)
+            angle -= 360;
+        return Mathf.Clamp(angle, yMin, yMax);
+    }
+
 }
+
