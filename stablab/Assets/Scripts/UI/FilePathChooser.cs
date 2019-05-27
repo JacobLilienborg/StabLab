9c1de9f0eb7dcd930a3a05a17502907d1fcbc3d2
Johan Fallström <avgrunden15@gmail.com>
2019-05-27 18:35:55 +0200

    Gjorde ett skript så att screenshot  path i settings fungerar korrekt

---
+++ stablab/Assets/Scripts/UI/FilePathChooser.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+// A simple class that lets one choose a file path, and changes the given InputField accordingly.
+public class FilePathChooser : MonoBehaviour
+{
+
+    public InputField filePath;
+
+    public void BrowseFilePath()
+    {
+        filePath.text = FileManager.OpenFileBrowser("", DataManager.instance.LoadApplicationData().recentWorkingDirectory);
+        filePath.onEndEdit.Invoke(filePath.text);
+    }
+
+}
